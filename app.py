f12a904b9a18abb6b1e787da65cbc92bb183716f
Hao Xu <xuh@email.unc.edu>
2018-07-11 14:28:51 -0400

    add text/tabular representation

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from jsonschema import validate, ValidationError
 from schema import cohort_schema, feature_association_schema, associations_to_all_features_schema
 from flasgger import Swagger
 import traceback
+from format import format_tabular
 
 with open('terms.txt', 'r') as content_file:
     terms_and_conditions = content_file.read()
@@ -26,6 +27,13 @@ template = {
     "description": "DDCR API [documentation](https://github.com/xu-hao/ddcr-api)",
     "version": "0.0.1"
   },
+  "consumes": [
+    "application/json"
+  ],
+  "produces": [
+    "application/json",
+    "text/tabular"
+  ],
   "host": "ddcr.renci.org",  # overrides localhost:500
   "basePath": "/",  # base bash for blueprint registration
   "schemes": [
@@ -37,7 +45,13 @@ swag = Swagger(app, template=template)
 
 @api.representation('application/json')
 def output_json(data, code, headers=None):
-    resp = make_response(json.dumps({"terms and conditions":terms_and_conditions, "return value":data}), code)
+    resp = make_response(json.dumps({"terms and conditions": terms_and_conditions, "return value": data}), code)
+    resp.headers.extend(headers or {})
+    return resp
+
+@api.representation('text/tabular')
+def output_tabular(data, code, headers=None):
+    resp = make_response(format_tabular(terms_and_conditions, data), code)
     resp.headers.extend(headers or {})
     return resp
 
---
+++ format.py
@@ -0,0 +1,92 @@
+def feature_to_text(feature_name, feature_qualifier):
+    return feature_name + " " + feature_qualifier["operator"] + " " + str(feature_qualifier["value"])
+
+
+def table_to_text(columns, rows):
+    rows = list(map(lambda i: list(map(str, i)), rows))
+    print(columns, rows)
+    if len(rows) == 0:
+        rowstrans = [[] for _ in columns]
+    else:
+        rowstrans = zip(*rows)
+    maxlens = [max([len(a)] + list(map(len, b))) for (a, b) in zip(columns, rowstrans)]
+    string = "|".join([b.ljust(a) for (a, b) in zip(maxlens, columns)])
+    string += "\n"
+    string += "+". join(["-" * l for l in maxlens])
+    string += "\n"
+    for row in rows:
+        string += "|".join([b.ljust(a) for (a, b) in zip(maxlens, row)])
+        string += "\n"
+    return string
+
+
+def format_tabular(term, data):
+    tables = []
+    format_tables(data, tables)
+    string = term
+    string += "\n"
+    for table in tables:
+        string += table_to_text(table[0], table[1])
+        string += "\n"
+    return string
+
+
+def format_tables(data, tables):
+    if isinstance(data, str):
+        columns = ["error"]
+        rows = [[str(data)]]
+        tables.append([columns, rows])
+    elif "features" in data:
+        columns = ["cohort_id", "size", "features"]
+        features = ",".join([feature_to_text(a, b) for (a, b) in data["features"].items()])
+        rows = [[data["cohort_id"], data["size"], features]]
+        tables.append([columns, rows])
+    elif "cohort_id" in data:
+        columns = ["cohort_id", "size"]
+        rows = [[data["cohort_id"], data["size"]]]
+        tables.append([columns, rows])
+    elif "feature_a" in data:
+        feature_a = data["feature_a"]
+        feature_b = data["feature_b"]
+        feature_a_feature_name = feature_a["feature_name"]
+        feature_a_feature_qualifiers = feature_a["feature_qualifiers"]
+        feature_b_feature_name = feature_b["feature_name"]
+        feature_b_feature_qualifiers = feature_b["feature_qualifiers"]
+
+        columns = ["feature"] + list(map(lambda x: feature_to_text(feature_a_feature_name, x), feature_a_feature_qualifiers))
+        rows = [[a] + b for (a, b) in zip(list(map(lambda x: feature_to_text(feature_b_feature_name, x), feature_b_feature_qualifiers)), data["feature_matrix"])]
+        tables.append([columns, rows])
+
+        columns = ["p_value", "chi_squared"]
+        rows = [[data["p_value"], data["chi_squared"]]]
+        tables.append([columns, rows])
+    elif "feature" in data:
+        feature = data["feature"]
+        feature_feature_name = feature["feature_name"]
+        feature_feature_qualifiers = feature["feature_qualifiers"]
+
+        columns = ["feature", "count"]
+        rows = [[a, b] for (a, b) in
+                zip(list(map(lambda x: feature_to_text(feature_feature_name, x), feature_feature_qualifiers)), data["feature_matrix"])]
+        tables.append([columns, rows])
+
+        columns = ["p_value", "chi_squared"]
+        rows = [[data["p_value"], data["chi_squared"]]]
+        tables.append([columns, rows])
+    elif isinstance(data, list):
+        for d in data:
+            format_tables(d, tables)
+    elif isinstance(data, dict):
+        columns = ["features"]
+        rows = [[feature_to_text(a, b)] for (a, b) in data.items()]
+        tables.append([columns, rows])
+    else:
+        columns = ["error"]
+        rows = [[str(data)]]
+        tables.append([columns, rows])
+
+
+
+
+
+

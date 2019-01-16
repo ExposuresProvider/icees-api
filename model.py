5baba8e4c649a67ea3f1ff9e388aceb3415a444e
Hao Xu <xuh@cs.unc.edu>
2019-01-16 12:21:48 -0500

    update

--- model.py
+++ model.py
@@ -88,7 +88,7 @@ def select_cohort(conn, table_name, year, cohort_features, cohort_id=None):
                 cohort_id = None
 
         if cohort_id_in_use(conn, cohort_id):
-            raise RuntimeException("Cohort id is in use.")
+            raise RuntimeError("Cohort id is in use.")
             ins = cohort.update().where(cohort.c.cohort_id == cohort_id).values(size=size,
                                                                     features=json.dumps(cohort_features,
                                                                                         sort_keys=True),

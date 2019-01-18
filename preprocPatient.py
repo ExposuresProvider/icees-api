9e59ddf061e7238efd2b385a84aa7508d05c949b
Hao Xu <xuh@cs.unc.edu>
2019-01-18 14:47:55 -0500

    binning

--- preprocPatient.py
+++ preprocPatient.py
@@ -1,30 +1,27 @@
 import pandas as pd
 import sys
+from preprocUtils import quantile, indices
 
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 year = sys.argv[3]
-
-
-def quantile(df, col, n):
-    df[col] = pd.qcut(df[col], n, labels=list(map(str, range(1,n+1))))
-
+binstr = sys.argv[4]
 
 df = pd.read_csv(input_file)
 
-quantile(df, "AvgDailyPM2.5Exposure", 5)
-quantile(df, "MaxDailyPM2.5Exposure", 5)
-quantile(df, "AvgDailyOzoneExposure", 5)
-quantile(df, "MaxDailyOzoneExposure", 5)
-quantile(df, "EstResidentialDensity", 5)
-quantile(df, "EstResidentialDensity25Plus", 5)
-quantile(df, "EstProbabilityNonHispWhite", 4)
-quantile(df, "EstProbabilityHouseholdNonHispWhite", 4)
-quantile(df, "EstProbabilityHighSchoolMaxEducation", 4)
-quantile(df, "EstProbabilityNoAuto", 4)
-quantile(df, "EstProbabilityNoHealthIns", 4)
-quantile(df, "EstProbabilityESL", 4)
-quantile(df, "EstHouseholdIncome", 5)
+quantile(df, "AvgDailyPM2.5Exposure", 5, binstr)
+quantile(df, "MaxDailyPM2.5Exposure", 5, binstr)
+quantile(df, "AvgDailyOzoneExposure", 5, binstr)
+quantile(df, "MaxDailyOzoneExposure", 5, binstr)
+df["EstResidentialDensity"] = pd.cut(df["EstResidentialDensity"], indices)
+quantile(df, "EstResidentialDensity25Plus", 5, binstr)
+quantile(df, "EstProbabilityNonHispWhite", 4, binstr)
+quantile(df, "EstProbabilityHouseholdNonHispWhite", 4, binstr)
+quantile(df, "EstProbabilityHighSchoolMaxEducation", 4, binstr)
+quantile(df, "EstProbabilityNoAuto", 4, binstr)
+quantile(df, "EstProbabilityNoHealthIns", 4, binstr)
+quantile(df, "EstProbabilityESL", 4, binstr)
+quantile(df, "EstHouseholdIncome", 5, binstr)
 df["MajorRoadwayHighwayExposure"] = pd.cut(df["MajorRoadwayHighwayExposure"], [-1, 0, 50, 100, 200, 300, 500], labels=list(map(str, [6, 1, 2, 3, 4, 5])))
 df["Mepolizumab"] = 0
 
---
+++ preprocUtils.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def quantile(df, col, n, bin="qcut"):
+    if bin == "qcut":
+        df[col] = pd.qcut(df[col], n, labels=list(map(str, range(1,n+1))))
+    elif bin == "cut":
+        df[col] = pd.cut(df[col], n, labels=list(map(str, range(1,n+1))))
+    else:
+        raise "unsupported binning method"
+
+indices = pd.IntervalIndex([pd.Interval(0, 2500, closed="left"),pd.Interval(2500, 50000, closed="left"), pd.Interval(50000, float("inf"), closed="left")])

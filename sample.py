a765154c32ef75f59938ba301fe2872dce9d6062
Hao Xu <xuh@cs.unc.edu>
2019-03-20 18:01:25 -0400

    sample

---
+++ sample.py
@@ -0,0 +1,24 @@
+from features2_0_0.features import features
+import pandas as pd
+import numpy as np
+from sqlalchemy import Integer, String, Enum
+import sys
+
+t = sys.argv[1]
+n = int(sys.argv[2])
+fn = sys.argv[3]
+
+df = pd.DataFrame({t[0].upper() + t[1:] + "ID":range(1,n+1)})
+
+for col, t, levels, _ in features[t]:
+    if levels is None:
+        if t == Integer:
+            df[col] = np.random.randint(10, size=n)
+        else:
+            print ("error: " + col)
+    else:
+        df[col] = np.random.choice(levels, size=n)
+
+
+df.to_csv(fn, index=False)
+

c7ef7e9803aa30e8e6bca0e1a0ad0fb555842a5f
Hao Xu <xuh@cs.unc.edu>
2019-04-09 15:42:14 -0400

    update

---
+++ utils.py
@@ -0,0 +1,22 @@
+def opposite(qualifier):
+    return {
+        "operator": {
+            ">": "<=",
+            "<": ">=",
+            ">=": "<",
+            "<=": ">",
+            "=": "<>",
+            "<>": "="
+        }[qualifier["operator"]],
+        "value": qualifier["value"]
+    }
+
+
+def to_qualifiers(feature):
+    k, v = list(feature.items())[0]
+    return {
+        "feature_name": k,
+        "feature_qualifiers": [v, opposite(v)]
+    }
+
+

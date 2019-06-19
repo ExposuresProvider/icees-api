ce3ccac8d234e39db7b2609f5b517e57dbf426f8
Hao Xu <xuh@cs.unc.edu>
2019-06-19 16:51:20 -0400

    update

--- sample.py
+++ sample.py
@@ -3,12 +3,23 @@ import pandas as pd
 import numpy as np
 from sqlalchemy import Integer, String, Enum
 import sys
+import argparse
 
-t = sys.argv[1]
-n = int(sys.argv[2])
-fn = sys.argv[3]
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('table', type=str)
+parser.add_argument('years', type=int, nargs="+")
+parser.add_argument('size', type=int)
+parser.add_argument('filename', type=str)
+
+args = parser.parse_args()
+t = args.table
+years = args.years
+n = args.size
+fn = args.filename
 
 df = pd.DataFrame({t[0].upper() + t[1:] + "Id":range(1,n+1)})
+
+df["year"] = np.random.choice(years, size=n)
 
 for col, t, levels, _ in features[t]:
     if levels is None:
@@ -18,7 +29,6 @@ for col, t, levels, _ in features[t]:
             print ("error: " + col)
     else:
         df[col] = np.random.choice(levels, size=n)
-
 
 df.to_csv(fn, index=False)
 

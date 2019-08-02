2eeb3306787e70b5987bd4224dd47b0d61836df7
Hao Xu <xuh@cs.unc.edu>
2019-08-02 15:24:26 -0400

    update

--- features1_0_0/model.py
+++ features1_0_0/model.py
@@ -49,7 +49,7 @@ def get_db_connection(version="1.0.0"):
     if hasattr(engine_map, version):
         engine = engine_map[version]
     else:
-        engine = create_engine("postgresql+psycopg2://"+serv_user+":"+serv_password+"@"+serv_host+":"+serv_port+"/"+serv_database[version])
+        engine = create_engine("postgresql+psycopg2://"+serv_user+":"+serv_password+"@"+serv_host+":"+serv_port+"/"+serv_database[version], pool_size=10, max_overflow=0)
         engine_map[version] = engine
 
     return engine.connect()

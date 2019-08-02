c7d791e34c2e01bfb121e55c9d9756721b312998
Hao Xu <xuh@cs.unc.edu>
2019-08-02 17:13:27 -0400

    update

--- model.py
+++ model.py
@@ -14,24 +14,24 @@ serv_database = json.loads(os.environ[service_name + "_DATABASE"])
 serv_max_overflow = int(os.environ[service_name + "_DB_MAX_OVERFLOW"])
 serv_pool_size = int(os.environ[service_name + "_DB_POOL_SIZE"])
 
-engine_map = {}
+# engine_map = {}
 
 def get_db_connection(version):
-    if hasattr(engine_map, version):
-        engine = engine_map[version]
-    else:
-        engine = create_engine("postgresql+psycopg2://"+serv_user+":"+serv_password+"@"+serv_host+":"+serv_port+"/"+serv_database[version], pool_size=serv_pool_size, max_overflow=serv_max_overflow)
-        engine_map[version] = engine
+    #if hasattr(engine_map, version):
+    #    engine = engine_map[version]
+    #else:
+    engine = create_engine("postgresql+psycopg2://"+serv_user+":"+serv_password+"@"+serv_host+":"+serv_port+"/"+serv_database[version], pool_size=serv_pool_size, max_overflow=serv_max_overflow)
+    #    engine_map[version] = engine
 
-    return engine.connect()
+    return engine
 
 
 @contextmanager
 def DBConnection(version):
     conn = get_db_connection(version)
     try:
-        yield conn
+        yield conn.connect()
     finally:
-        conn.close()
+        conn.dispose()
 
 

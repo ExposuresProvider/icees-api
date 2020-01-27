aba509a45d701c0a7b9137c2291745be335a09d3
Patrick Wang <patrick@covar.com>
2020-01-26 20:35:47 -0500

    remove version

--- dbutils.py
+++ dbutils.py
@@ -6,17 +6,17 @@ import db
 from features import model, features
 
 def createargs(args):
-    create(args.version)
+    create()
 
-def create(version):
-    with db.DBConnection(version) as conn:
+def create():
+    with db.DBConnection() as conn:
         with conn.begin() as trans:
-            model[version].metadata.create_all(conn)
+            model.metadata.create_all(conn)
     
 def insertargs(args):
-    insert(args.version, args.input_file, args.table_name)
+    insert(args.input_file, args.table_name)
 
-def insert(version, input_file, table_name):
+def insert(input_file, table_name):
     df = pd.read_csv(input_file)
     def toDType(table):
         print(table)
@@ -25,15 +25,14 @@ def insert(version, input_file, table_name):
             l.append((col_name, col_type))
         return dict(l)
         
-    with db.DBConnection(version) as conn:
+    with db.DBConnection() as conn:
         with conn.begin() as trans:
             df.to_sql(name=table_name, con=conn,if_exists="append", 
                       index=False, 
-                      dtype=toDType(features[version].features[table_name]))
+                      dtype=toDType(features.features[table_name]))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog='ICEES DB Utitilies')
-    parser.add_argument('--version', required=True, type=str, help='version of the database')
     subparsers = parser.add_subparsers(help='subcommands')
     # create the parser for the "create" command
     parser_create = subparsers.add_parser('create', help='create tables')
--- test/test_api.py
+++ test/test_api.py
@@ -14,7 +14,6 @@ wait = os.environ.get("WAIT")
 if wait is not None:
     time.sleep(int(wait))
 
-version = "2.0.0"
 table = "patient"
 year = 2010
 tabular_headers = {"Content-Type" : "application/json", "accept": "text/tabular"}
@@ -78,50 +77,9 @@ def query(year, biolink_class):
             }
         }
 
-    
-def do_test_knowledge_graph(biolink_class):
-
-        resp = requests.post(prot + "://"+host+":"+str(port)+"/{0}/knowledge_graph".format(version), data = json.dumps(query(year, biolink_class)), headers = json_headers, verify = False)
-        resp_json = resp.json()
-        assert "return value" in resp_json
-        assert "n_results" in resp_json["return value"]
-        assert "answers" in resp_json["return value"]
-        assert resp_json["return value"]["n_results"] == len(resp_json["return value"]["answers"])
-        assert "knowledge_graph" in resp_json["return value"]
-        assert "message_code" in resp_json["return value"]
-        assert "tool_version" in resp_json["return value"]
-        assert "datetime" in resp_json["return value"]
-
-
-def do_test_knowledge_graph_unique_edge_ids(biolink_class):
-
-        resp = requests.post(prot + "://"+host+":"+str(port)+"/{0}/knowledge_graph".format(version), data = json.dumps(query(year, biolink_class)), headers = json_headers, verify = False)
-        resp_json = resp.json()
-        assert "return value" in resp_json
-
-        for edge_bindings in map(lambda x: x["edge_bindings"], resp_json["return value"]["answers"]):
-            assert "e00" in edge_bindings
-            assert len(edge_bindings) == 1
-            assert len(edge_bindings["e00"]) == 1
-
-        edge_ids = list(map(lambda x: x["edge_bindings"]["e00"][0], resp_json["return value"]["answers"]))
-        assert len(edge_ids) == len(set(edge_ids))
-
-
-def do_test_knowledge_graph_edge_set(biolink_class):
-
-        resp = requests.post(prot + "://"+host+":"+str(port)+"/{0}/knowledge_graph".format(version), data = json.dumps(query(year, biolink_class)), headers = json_headers, verify = False)
-        resp_json = resp.json()
-        assert "return value" in resp_json
-
-        edge_ids = set(map(lambda x: x["edge_bindings"]["e00"][0], resp_json["return value"]["answers"]))
-        edge_ids2 = set(map(lambda x: x["id"], resp_json["return value"]["knowlegde_graph"]["edges"]))
-        assert edge_ids == edge_ids2
-
-
 def do_test_get_identifiers(i):
         feature_variables = {}
-        resp = requests.get(prot + "://"+host+":"+str(port)+"/{0}/{1}/{2}/identifiers".format(version, table, i), headers = json_headers, verify = False)
+        resp = requests.get(prot + "://"+host+":"+str(port)+"/{0}/{1}/identifiers".format(table, i), headers = json_headers, verify = False)
         resp_json = resp.json()
         assert "return value" in resp_json
         assert "identifiers" in resp_json["return value"]
@@ -130,7 +88,7 @@ def do_test_get_identifiers(i):
 
 def test_post_cohort():
         feature_variables = {}
-        resp = requests.post(prot + "://"+host+":"+str(port)+"/{0}/{1}/{2}/cohort".format(version, table, year), data=json.dumps(feature_variables), headers = json_headers, verify = False)
+        resp = requests.post(prot + "://"+host+":"+str(port)+"/{0}/{1}/cohort".format(table, year), data=json.dumps(feature_variables), headers = json_headers, verify = False)
         resp_json = resp.json()
         assert "return value" in resp_json
         assert "cohort_id" in resp_json["return value"]
@@ -138,10 +96,10 @@ def test_post_cohort():
 
 def test_cohort_dictionary():
         feature_variables = {}
-        resp = requests.post(prot + "://"+host+":"+str(port)+"/{0}/{1}/{2}/cohort".format(version, table, year), data=json.dumps(feature_variables), headers = json_headers, verify = False)
+        resp = requests.post(prot + "://"+host+":"+str(port)+"/{0}/{1}/cohort".format(table, year), data=json.dumps(feature_variables), headers = json_headers, verify = False)
         resp_json = resp.json()
 
-        resp1 = requests.get(prot + "://"+host+":"+str(port)+"/{0}/{1}/{2}/cohort/dictionary".format(version, table, year), headers = json_headers, verify = False)
+        resp1 = requests.get(prot + "://"+host+":"+str(port)+"/{0}/{1}/cohort/dictionary".format(table, year), headers = json_headers, verify = False)
         resp_json1 = resp1.json()
         assert {
             "features": {}, 
@@ -149,41 +107,6 @@ def test_cohort_dictionary():
             "size": resp_json["return value"]["size"]
         } in resp_json1["return value"]
    
-def test_knowledge_graph_schema():
-        resp = requests.get(prot + "://"+host+":"+str(port)+"/{0}/knowledge_graph/schema".format(version), headers = json_headers, verify = False)
-        resp_json = resp.json()
-        assert "return value" in resp_json
-        assert "population_of_individual_organisms" in resp_json["return value"]
-        assert "chemical_substance" in resp_json["return value"]["population_of_individual_organisms"]
-        assert "association" in resp_json["return value"]["population_of_individual_organisms"]["chemical_substance"]
-
-def test_knowledge_graph_for_chemical_substance():
-        do_test_knowledge_graph("chemical_substance")
-
-def test_knowledge_graph_for_phenotypic_feature():
-        do_test_knowledge_graph("phenotypic_feature")
-
-def test_knowledge_graph_for_disease():
-        do_test_knowledge_graph("disease")
-
-def test_knowledge_graph_unique_edge_ids_for_chemical_substance():
-        do_test_knowledge_graph_unique_edge_ids("chemical_substance")
-
-def test_knowledge_graph_unique_edge_ids_for_phenotypic_feature():
-        do_test_knowledge_graph_unique_edge_ids("phenotypic_feature")
-
-def test_knowledge_graph_unique_edge_ids_for_disease():
-        do_test_knowledge_graph_unique_edge_ids("disease")
-
-def test_knowledge_graph_edge_set_for_chemical_substance():
-        do_test_knowledge_graph_unique_edge_ids("chemical_substance")
-
-def test_knowledge_graph_edge_set_for_phenotypic_feature():
-        do_test_knowledge_graph_unique_edge_ids("phenotypic_feature")
-
-def test_knowledge_graph_edge_set_for_disease():
-        do_test_knowledge_graph_unique_edge_ids("disease")
-
 def test_get_identifiers_for_ObesityDx():
         do_test_get_identifiers("ObesityDx")
 
@@ -198,7 +121,7 @@ def test_get_identifiers_OvarianCancerDx():
 
 def test_associations_to_all_features2():
         feature_variables = {}
-        resp = requests.post(prot + "://"+host+":"+str(port)+"/{0}/{1}/{2}/cohort".format(version, table, year), data=json.dumps(feature_variables), headers = json_headers, verify = False)
+        resp = requests.post(prot + "://"+host+":"+str(port)+"/{0}/{1}/cohort".format(table, year), data=json.dumps(feature_variables), headers = json_headers, verify = False)
         resp_json = resp.json()
         cohort_id = resp_json["return value"]["cohort_id"]
         atafdata = {
@@ -206,51 +129,18 @@ def test_associations_to_all_features2():
                 "AgeStudyStart": list(map(lambda x: {
                     "operator": "=",
                     "value": x
-                }, features[version].age_levels))
+                }, features.age_levels))
             },
             "maximum_p_value": 1
         }
-        resp = requests.post(prot + "://"+host+":"+str(port)+"/{0}/{1}/{2}/cohort/{3}/associations_to_all_features2".format(version, table, year, cohort_id), data=json.dumps(atafdata), headers = json_headers, verify = False)
+        resp = requests.post(prot + "://"+host+":"+str(port)+"/{0}/{1}/cohort/{2}/associations_to_all_features2".format(table, year, cohort_id), data=json.dumps(atafdata), headers = json_headers, verify = False)
         resp_json = resp.json()
         assert "return value" in resp_json
         assert isinstance(resp_json["return value"], list)
 
-def test_associations_to_all_features2b_v2():
-    version = "2.0.0"
+def test_associations_to_all_features2b():
     feature_variables = {}
-    resp = requests.post(prot + "://"+host+":"+str(port)+"/{0}/{1}/{2}/cohort".format(version, table, year), data=json.dumps(feature_variables), headers = json_headers, verify = False)
-    resp_json = resp.json()
-    cohort_id = resp_json["return value"]["cohort_id"]
-    atafdata = {
-            "feature": {
-                "AgeStudyStart": [
-                    {
-                        "operator": "=",
-                        "value": "0-2"
-                    }, {
-                        "operator": "between",
-                        "value_a": "3-17", 
-                        "value_b": "18-34"
-                    }, {
-                        "operator":"in", 
-                        "values":["35-50","51-69"]
-                    }, {
-                        "operator":"=",
-                        "value":"70-89"
-                    }
-                ]
-            },
-            "maximum_p_value": 1
-        }
-    resp = requests.post(prot + "://"+host+":"+str(port)+"/{0}/{1}/{2}/cohort/{3}/associations_to_all_features2".format(version, table, year, cohort_id), data=json.dumps(atafdata), headers = json_headers, verify = False)
-    resp_json = resp.json()
-    assert "return value" in resp_json
-    assert isinstance(resp_json["return value"], list)
-
-def test_associations_to_all_features2b_v1():
-    version = "1.0.0"
-    feature_variables = {}
-    resp = requests.post(prot + "://"+host+":"+str(port)+"/{0}/{1}/{2}/cohort".format(version, table, year), data=json.dumps(feature_variables), headers = json_headers, verify = False)
+    resp = requests.post(prot + "://"+host+":"+str(port)+"/{0}/{1}/cohort".format(table, year), data=json.dumps(feature_variables), headers = json_headers, verify = False)
     resp_json = resp.json()
     cohort_id = resp_json["return value"]["cohort_id"]
     atafdata = {
@@ -274,7 +164,7 @@ def test_associations_to_all_features2b_v1():
             },
             "maximum_p_value": 1
         }
-    resp = requests.post(prot + "://"+host+":"+str(port)+"/{0}/{1}/{2}/cohort/{3}/associations_to_all_features2".format(version, table, year, cohort_id), data=json.dumps(atafdata), headers = json_headers, verify = False)
+    resp = requests.post(prot + "://"+host+":"+str(port)+"/{0}/{1}/cohort/{2}/associations_to_all_features2".format(table, year, cohort_id), data=json.dumps(atafdata), headers = json_headers, verify = False)
     resp_json = resp.json()
     assert "return value" in resp_json
     assert isinstance(resp_json["return value"], list)

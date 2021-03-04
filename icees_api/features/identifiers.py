f290c0bf185c4a15f23648f2bad14665df569cb2
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-03-04 16:16:39 -0500

    Clean up identifiers.py

--- icees_api/features/identifiers.py
+++ icees_api/features/identifiers.py
@@ -1,4 +1,4 @@
-import csv
+"""Identifiers."""
 import logging
 import os
 
@@ -14,28 +14,31 @@ input_file = os.path.join(get_config_path(), "identifiers.yml")
 with open(input_file) as inpf:
     input_dict = yaml.safe_load(inpf)
 
+
 def get_identifiers(table, feature, return_empty_list=False):
+    """Get identifiers."""
     if table in input_dict:
         identifier_dict = input_dict[table]
     else:
         raise RuntimeError("Cannot find table " + table)
-    if feature in identifier_dict:
-        return identifier_dict[feature]
-    else:
+    if feature not in identifier_dict:
         errmsg = "Cannot find identifiers for feature " + feature
         logger.error(errmsg)
         if return_empty_list:
             return []
         else:
             raise RuntimeError(errmsg)
+    return identifier_dict[feature]
 
-        
+
 def get_features_by_identifier(table, identifier):
-    if table in input_dict:
-        identifier_dict = input_dict[table]
-    else:
+    """Get features by identifier."""
+    if table not in input_dict:
         raise Left(f"Cannot find table {table}, available {input_dict}")
+    identifier_dict = input_dict[table]
 
-    return Right([feature for feature, identifiers in identifier_dict.items() if identifier in identifiers])
-        
-
+    return Right([
+        feature
+        for feature, identifiers in identifier_dict.items()
+        if identifier in identifiers
+    ])

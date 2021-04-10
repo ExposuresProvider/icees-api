304fa229270739e84c9e3d5230a442dcfcf88444
Hao Xu <xuh@cs.unc.edu>
2021-04-09 23:01:15 -0400

    update qc

--- config/qc.py
+++ config/qc.py
@@ -5,9 +5,122 @@ import argparse
 from prettytable import PrettyTable
 import difflib
 from colorama import init, Fore, Back, Style
+from itertools import chain
 
 init()
 
+
+def update_key(d, ok, nk):
+    i = list(d.keys()).index(ok)
+    d.insert(i, nk, d.pop(ok))
+    
+class FeaturesFile:
+
+    def __init__(self, filename):
+        self.yaml = YAML(typ="rt")
+        with open(filename) as inf:
+            self.obj = self.yaml.load(inf)
+
+    def get_keys(self, table):
+        return self.obj[table].keys()
+        
+    def update_key(self, table, old_key, new_key):
+        update_key(self.obj[table], old_key, new_key)
+
+    def dump(self, filename):
+        with open(filename, "w+") as of:
+            self.yaml.dump(self.obj, of)
+        
+
+class IdentifiersFile:
+
+    def __init__(self, filename):
+        self.yaml = YAML(typ="rt")
+        with open(filename) as inf:
+            self.obj = self.yaml.load(inf)
+
+    def get_keys(self, table):
+        return self.obj[table].keys()
+        
+    def update_key(self, table, old_key, new_key):
+        update_key(self.obj[table], old_key, new_key)
+
+    def dump(self, filename):
+        with open(filename, "w+") as of:
+            self.yaml.dump(self.obj, of)
+        
+
+class MappingFile:
+
+    def __init__(self, filename):
+        self.yaml = YAML(typ="rt")
+        with open(filename) as inf:
+            self.obj = self.yaml.load(inf)
+
+    def get_sub_objects(self):
+        FHIR = self.obj.get("FHIR", {})
+        GEOID = self.obj.get("GEOID", {})
+        NearestRoad = self.obj.get("NearestRoad", {})
+        NearestPoint = self.obj.get("NearestPoint", {})
+        Visit = self.obj.get("Visit", {})
+        return FHIR, GEOID, NearestRoad, NearestPoint, Visit
+
+    def get_sub_keys(self, FHIR, GEOID, NearestRoad, NearestPoint, Visit):
+        FHIR_keys = list(FHIR.keys())
+        GEOID_keys = {name: list(dataset["columns"].values()) for name, dataset in GEOID.items()}
+        NearestRoad_keys = {name: list(map(lambda x: x["feature_name"], dataset["attributes_to_features_map"].values())) for name, dataset in NearestRoad.items()}
+        NearestPoint_keys = {name: list(map(lambda x: x["feature_name"], dataset["attributes_to_features_map"].values())) for name, dataset in NearestPoint.items()}
+        Visit_keys = []
+        return FHIR_keys, GEOID_keys, NearestRoad_keys, NearestPoint_keys, Visit_keys
+    
+    def get_keys(self, table):
+        FHIR_keys, GEOID_keys, NearestRoad_keys, NearestPoint_keys, Visit_keys = self.get_sub_keys(*self.get_sub_objects())
+        return FHIR_keys + list(chain(GEOID_keys.values())) + list(chain(NearestRoad_keys.values())) + list(chain(NearestPoint_keys.values())) + Visit_keys
+        
+    def update_key(self, table, old_key, new_key):
+        FHIR, GEOID, NearestRoad, NearestPoint, Visit = self.get_sub_objects()
+        FHIR_keys, GEOID_keys, NearestRoad_keys, NearestPoint_keys, Visit_keys = self.get_sub_keys(FHIR, GEOID, NearestRoad, NearestPoint, Visit)
+        if old_key in FHIR_keys:
+            update_key(self.obj["FHIR"], old_key, new_key)
+            return
+
+        for name, keys in GEOID_keys.items():
+            if old_key in keys:
+                for column_name, var_name in GEOID[name]["columns"].items():
+                    if var_name == old_key:
+                        GEOID[name]["columns"][column_name] = new_key
+                        return
+
+        for name, keys in NearestRoad_keys.items():
+            if old_key in keys:
+                for attribute_name, feature in NearestRoad[name]["attributes_to_features_map"].items():
+                    if feature["feature_name"] == old_key:
+                        NearestRoad[name]["attributes_to_features_map"][attribute_name]["feature_name"] = new_key
+                        return
+        
+        for name, keys in NearestPoint_keys.items():
+            if old_key in keys:
+                for attribute_name, feature in NearestPoint[name]["attributes_to_features_map"].items():
+                    if feature["feature_name"] == old_key:
+                        NearestPoint[name]["attributes_to_features_map"][attribute_name]["feature_name"] = new_key
+                        return
+        
+        print("variable {old_key} no longer exists")
+
+    def dump(self, filename):
+        with open(filename, "w+") as of:
+            self.yaml.dump(self.obj, of)
+
+
+def make_file(ty, filename):
+    if ty == "features":
+        return FeaturesFile(filename)
+    elif ty == "mapping":
+        return MappingFile(filename)
+    elif ty == "identifiers":
+        return IdentifiersFile(filename)
+
+    
 def difference_ignore_suffix(a, b, ignore_suffix):
     diff = []
     for an in a:
@@ -22,20 +135,28 @@ def difference_ignore_suffix(a, b, ignore_suffix):
             
     
 def truncate_set(a, b, n, ignore_suffix):
-    diff = difference_ignore_suffix(a, b, ignore_suffix)
-    l = list(diff)
+    diff_a = difference_ignore_suffix(a, b, ignore_suffix)
+    diff_b = difference_ignore_suffix(b, a, ignore_suffix)
 
-    levenshtein = lambda x: x[1]
-    ls0 = sorted([(an, max([(bn, Levenshtein.ratio(an, bn)) for bn in b], key=levenshtein)) for an in l], reverse=True, key=lambda t: levenshtein(t[1]))
+    def find_match(b, an):
+        bn, ratio = max([(bn, Levenshtein.ratio(an, bn)) for bn in b], key=lambda x: x[1])
+        return (an, bn, ratio)
+
+    diff_a_match = [find_match(b, an) for an in diff_a]
+    diff_b_match = [find_match(a, bn) for bn in diff_b]
+
+    diff_b_match_switched = [(an, bn, ratio) for bn, an, ratio in diff_b_match]
+
+    diff_match = sorted(list(set(diff_a_match) | set(diff_b_match_switched)), reverse=True, key=lambda t: t[2])
     
-    ls = [(an, (bn, ratio)) if ratio >= similarity_threshold else (an, None) for an, (bn, ratio) in ls0]
+    ls = [(an, bn, ratio) if ratio >= similarity_threshold else (an, None, None) for an, bn, ratio in diff_match]
 
     if n == -1:
         topn = ls
     else:
         topn = ls[:n]
         
-    return [[t[0], t[1][0], t[1][1]] if t[1] is not None else [t[0], "", ""] for t in topn], n >= 0 and len(ls) > n
+    return topn, n >= 0 and len(ls) > n
 
 
 def colorize_diff(a, b):
@@ -61,105 +182,136 @@ def colorize_diff(a, b):
     return (a_colorized, b_colorized)
     
     
+def f(l):
+    if l[1] is None:
+        return [l[0], "", ""]
+    else:
+        return list(colorize_diff(l[0], l[1])) + [f"{l[2]:.2f}"]
+
+
 def print_matches(left, right, table, ellipsis):
     table_copy = list(table)
     if ellipsis:
-        table_copy.append(["...", "", ""])
+        table_copy.append(["...", None, None])
 
     x = PrettyTable()
     x.field_names = [left, right, "ratio"]
-    def f(l):
-        if l[1] == "":
-            return [l[0], l[1], l[2]]
-        else:
-            return list(colorize_diff(l[0], l[1])) + [f"{l[2]:.2f}"]
         
     x.add_rows(map(f, table_copy))
     print(x)
     
     
+def interactive_update(left, right, a_file, b_file, table_name, table, ellipsis):
+    done = False
+    n = len(table)
+    for i, row in enumerate(table):
+
+        print(f"{i+1} / {n}")
+        x = PrettyTable()
+        x.field_names = [left, right, "ratio"]
+        x.add_row(f(row))
+        
+        print(x)
+
+        while True:
+            if b_file is not None:
+                print("a) use a")
+            if a_file is not None:
+                print("b) use b")
+                if b_file is not None:
+                    print("c) customize")
+            print("""s) skip
+e) exit""")
+            action = input()
+
+            if action == "a":
+                b_file.update_key(table_name, row[1], row[0])
+                break
+            elif action == "b":
+                a_file.update_key(table_name, row[0], row[1])
+                break
+            elif action == "c":
+                var_name = input("input variable name: ")
+                a_file.update_key(table_name, row[0], var_name)
+                b_file.update_key(table_name, row[1], var_name)
+                break
+            elif action == "s":
+                break
+            elif action == "e":
+                done = True
+                break
+            else:
+                print("unsupported action, try again")
+        if done:
+            break
+                
+    if ellipsis:
+        print("more diff remaining")
+    
 parser = argparse.ArgumentParser(description='ICEES FHIR-PIT QC Tool')
-parser.add_argument('--features', metavar='FEATURES', type=str, required=True,
-                    help='yaml file for ICEES features')
-parser.add_argument('--mapping', metavar='MAPPING', type=str, required=True,
-                    help='yaml file for mapping from FHIR, etc. to ICEES features')
-parser.add_argument('--identifiers', metavar='IDENTIFIERS', type=str, required=True,
-                    help='yaml file for identifiers for ICEES features')
+parser.add_argument('--a', metavar='A', type=str,
+                    help='file a')
+parser.add_argument('--b', metavar='B', type=str,
+                    help='file b')
+parser.add_argument('--a_type', metavar='A_TYPE', choices=["features", "mapping", "identifiers"], required=True,
+                    help='type of file a')
+parser.add_argument('--b_type', metavar='B_TYPE', choices=["features", "mapping", "identifiers"], required=True,
+                    help='type of file b')
 parser.add_argument('-n', metavar='N', type=int, default=-1,
                     help='number of entries to display, -1 for unlimited')
 parser.add_argument('--ignore_suffix', metavar='IGNORE_SUFFIX', type=str, default="",
                     help='the suffix to ignore')
 parser.add_argument("--similarity_threshold", metavar="SIMILARITY_THRESHOLD", type=float, default=0.5,
                     help="the threshold for similarity suggestions")
-parser.add_argument("--update_features", metavar="UPDATE_FEATURES", type=str,
-                    help="yaml file for the updated features. if this file is not specified then the features cannot be updated")
-parser.add_argument("--update_mapping", metavar="UPDATE_MAPPING", type=str,
-                    help="yaml file for the updated mapping. if this file is not specified then the mapping cannot be updated")
-parser.add_argument("--update_identifiers", metavar="update_identifiers", type=str,
-                    help="yaml file for the updated identifiers. if this file is not specified then the identifiers cannot be updated")
+parser.add_argument('--table', metavar='TABLE', type=str, required=True, nargs="+",
+                    help='tables')
+parser.add_argument("--update_a", metavar="UPDATE_A", type=str,
+                    help="yaml file for the updated a. if this file is not specified then a cannot be updated")
+parser.add_argument("--update_b", metavar="UPDATE_B", type=str,
+                    help="yaml file for the updated b. if this file is not specified then b cannot be updated")
 
 args = parser.parse_args()
 
-features = args.features
-mapping = args.mapping
-identifiers = args.identifiers
+a_filename = args.a
+a_type = args.a_type
+a_update = args.update_a
+
+b_filename = args.b
+b_type = args.b_type
+b_update = args.update_b
+
+tables = args.table
 n = args.n
 ignore_suffix = args.ignore_suffix
 similarity_threshold = args.similarity_threshold
-update_features = args.update_features
-update_mapping = args.update_mapping
-update_identifiers = args.update_identifiers
 
-interactive_mapping = update_features is not None or update_mapping is not None
-interacitve_identifiers = update_features is not None or update_identifiers is not None
-
-yaml = YAML(typ="safe")
+interactive = a_update is not None or b_update is not None
 
 try:
-    with open(features) as ff:
-        features_obj = yaml.load(ff)
+    a_file = make_file(a_type, a_filename)
 except Exception as e:
-    print(f"error loading features yaml: {e}")
+    print(f"error loading {a_filename}: {e}")
     sys.exit(-1)
 
 try:
-    with open(mapping) as fm:
-        mapping_obj = yaml.load(fm)
+    b_file = make_file(b_type, b_filename)
 except Exception as e:
-    print(f"error loading mapping yaml: {e}")
+    print(f"error loading {b_filename}: {e}")
     sys.exit(-1)
 
-try:
-    with open(identifiers) as fi:
-        identifiers_obj = yaml.load(fi)
-except Exception as e:
-    print(f"error loading identifiers yaml: {e}")
-    sys.exit(-1)
-
-try:
-    mapping_var_names = set(mapping_obj["FHIR"].keys())
-except Exception as e:
-    print(f"error reading mapping yaml: {e}")
-    sys.exit(-1)
-    
-for table, table_features in features_obj.items():
-    table_features_var_names = set(table_features.keys())
+for table in tables:
+    a_var_names = a_file.get_keys(table)
+    b_var_names = b_file.get_keys(table) 
     print(f"feature vars table {table}:")
-    print(f"mapping diff features:")
-    print_matches("mapping", "features", *truncate_set(mapping_var_names, table_features_var_names, n, ignore_suffix))
-#    print(f"features diff mapping:")
-#    print_matches("features", "mapping", *truncate_set(table_features_var_names, mapping_var_names, n, ignore_suffix))
-
-    try:
-        identifiers_var_names = set(identifiers_obj[table].keys())
-    except Exception as e:
-        print(f"error reading identifiers yaml: {e}")
-        sys.exit(-1)
-
-    print(f"identifiers diff features:")
-    print_matches("identifiers", "features", *truncate_set(identifiers_var_names, table_features_var_names, n, ignore_suffix))
-#    print(f"features diff identifiers:")
-#    print_matches("features", "identifiers", *truncate_set(table_features_var_names, identifiers_var_names, n, ignore_suffix))
+    print(f"{a_filename} diff {b_filename}:")
+    if interactive:
+        interactive_update(a_filename, b_filename, a_file, b_file, table, *truncate_set(a_var_names, b_var_names, n, ignore_suffix))
+        if a_update is not None:
+            a_file.dump(a_update)
+        if b_update is not None:
+            b_file.dump(b_update)
+    else:
+        print_matches(a_filename, b_filename, *truncate_set(a_var_names, b_var_names, n, ignore_suffix))
 
     
 

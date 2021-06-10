1f5a120f9ad2785d396a7779bd0149f270137c68
Hao Xu <xuh@cs.unc.edu>
2021-06-10 16:51:57 -0400

    add bash script for running test

---
+++ test.sh
@@ -0,0 +1,7 @@
+docker run --name redis -d --net host --rm redis
+export CONFIG_PATH=./test/config
+export DB_PATH=./test/example.db
+export ICEES_API_LOG_PATH=./logs
+mkdir ./logs
+python -m pytest --cov=icees_api --cov-report=xml test/
+docker rm -f redis

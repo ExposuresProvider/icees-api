67222d5239a16f85854c7eed930643a028cad82b
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-07-21 11:11:29 -0400

    Add support for operations/workflow

--- icees_api/trapi.py
+++ icees_api/trapi.py
@@ -63,6 +63,9 @@ class TRAPI(FastAPI):
                 "description": "The values for version are restricted according to the regex in this external JSON schema. See schema and examples at url",
                 "url": "https://github.com/NCATSTranslator/translator_extensions/blob/production/x-trapi/",
             },
+            "operations": [
+                "lookup",
+            ],
         }
         if self.trapi_operations:
             openapi_schema["info"]["x-trapi"]["operations"] = self.trapi_operations

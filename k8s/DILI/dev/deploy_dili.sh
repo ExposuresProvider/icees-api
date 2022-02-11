8f8b0ac4f8cf4b4636fc6f2ba967477040104902
hyi <hongyi@renci.org>
2022-02-11 12:37:05 -0500

    get DILI service deployed with DILI specific names to avoid conflicts with other ICEES services

--- k8s/DILI/dev/deploy_dili.sh
+++ k8s/DILI/dev/deploy_dili.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 kubectl apply -f dili-config.yaml
-kubectl apply -f server-service.yaml,redis-service.yaml,server-deployment.yaml,redis-deployment.yaml
+kubectl apply -f server-service.yaml,redis-service.yaml,server-deployment.yaml,redis-deployment.yaml,server-ingress.yaml

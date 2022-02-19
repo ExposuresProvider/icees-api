4636cad9dff28eaeca313f0b298d0c0221f3e76c
hyi <hongyi@renci.org>
2022-02-19 17:06:20 -0500

    improved asthma and PCD deployment by adding liveness probe, redis persistent volume, etc

--- k8s/asthma/dev/deploy_asthma.sh
+++ k8s/asthma/dev/deploy_asthma.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 kubectl --namespace icees-dev apply -f asthma-config.yaml
-kubectl --namespace icees-dev apply -f server-service.yaml,redis-service.yaml,server-deployment.yaml,redis-deployment.yaml,server-ingress.yaml
+kubectl --namespace icees-dev apply -f redis-pvc.yaml,server-service.yaml,redis-service.yaml,server-deployment.yaml,redis-deployment.yaml,server-ingress.yaml
--- k8s/asthma/prod/deploy_asthma.sh
+++ k8s/asthma/prod/deploy_asthma.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 kubectl --namespace icees-prod apply -f asthma-config.yaml
-kubectl --namespace icees-prod apply -f server-service.yaml,redis-service.yaml,server-deployment.yaml,redis-deployment.yaml,server-ingress.yaml
+kubectl --namespace icees-prod apply -f redis-pvc.yaml,server-service.yaml,redis-service.yaml,server-deployment.yaml,redis-deployment.yaml,server-ingress.yaml

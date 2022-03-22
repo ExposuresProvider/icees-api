33ccb3f9815e2879567e9d0dda03acf5971a739e
Hong Yi <hongyi@renci.org>
2022-03-22 13:40:46 -0400

    remove k8s spec now that ICEES TRAPI services have been deployed with helm charts (#232)

--- k8s/DILI/dev/deploy_dili.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl --namespace icees-dev apply -f dili-config.yaml
-kubectl --namespace icees-dev apply -f redis-pvc.yaml,server-service.yaml,redis-service.yaml,server-deployment.yaml,redis-deployment.yaml,server-ingress.yaml
--- k8s/PCD/dev/deploy_pcd.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl --namespace icees-dev apply -f pcd-config.yaml
-kubectl --namespace icees-dev apply -f redis-pvc.yaml,server-service.yaml,redis-service.yaml,server-deployment.yaml,redis-deployment.yaml,server-ingress.yaml
